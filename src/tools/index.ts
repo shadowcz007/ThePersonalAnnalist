1b0cde86f907c1cdef57697f91fc2a11af4c7678
shadowcz007 <chizhiwei007@163.com>
2025-06-11 19:38:11 +0800

    增加采访者

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,7 +1,16 @@
 import './init'
 import { createTool } from './base'
 import personal_thought from './personal_thought.json'
+import interview_host from './interview_host.json'
 
-const personal_thought_tool = createTool(personal_thought as unknown as ToolDefinition)
+const personal_thought_tool = createTool(
+  personal_thought as unknown as ToolDefinition
+)
+const interview_host_tool = createTool(
+  interview_host as unknown as ToolDefinition
+)
 
-export const tools: any = Object.values(personal_thought_tool) 
+export const tools: any = [
+  ...Object.values(personal_thought_tool),
+  ...Object.values(interview_host_tool)
+]
